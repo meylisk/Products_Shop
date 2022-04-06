e51f042d77e3b66d5df395885bfb40cfdd6de460
Meylis Garryyev <meylisk@gmail.com>
2022-04-05 20:05:57 -0700

    changed files

--- script.js
+++ script.js
@@ -38,27 +38,6 @@ const getAllProducts = async () => {
 getAllProducts()
 
 
-// Here we are getting random joke from Chuck Norris api. We also apply name from our text fields.
-// giveMeanewJoke.addEventListener('click',()=>{
-//    // fetch("https://api.chucknorris.io/jokes/random").then(response => response.json()).then(data => joke.innerHTML = data.value).catch(err=> console.log(err.message))
-//    betterCallToApi()
-// })
-
-// const betterCallToApi = async () =>{
-//     try{
-
-//     const req = await fetch(`https://api.chucknorris.io/jokes/random?name=${firstName.value}${lastName.value}`)
-//     const data = await req.json()
-
-//     joke.innerHTML = data.value
-//     }catch(err){
-//         console.log(err.message)
-//     }
-// }
-
-
-
-
 
 
 
